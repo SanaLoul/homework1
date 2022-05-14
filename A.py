8175e9b0614e83e58d594ef4286b6a61e18d5568
SanaLoul <sanaloul3@gmail.com>
2022-05-14 02:56:25 -0600

    first commit

---
+++ A.py
@@ -0,0 +1,6 @@
+l=['Sana','Rana','Ali','Malak','Hala']
+sname=input('Inter the student name:')
+if sname in l:
+    print('The student name: ',sname,' is gradute')
+else:
+    print("This student name:",sname,"  doesn't gradute")
